cc26a2de051a7380982970c40d8d5ec7806a830f
cekmop_npu3 <milanastrunevskaya@gmail.com>
2022-12-02 21:28:41 +0300

    test

---
+++ src/components/templates/AppTemplate/AppTemplate.js
@@ -0,0 +1,10 @@
+import { Component } from '../../../core';
+import '../../Header';
+
+export class AppTemplate extends Component {
+	render() {
+		return `
+         <it-header></it-head>
+      `;
+	}
+}
